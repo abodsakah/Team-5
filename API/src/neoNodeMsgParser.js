ae5d737ac0824e052c2200410c73eafc87dbbd2f
Noah <nallephu12@gmail.com>
2022-02-15 10:10:58 +0100

    started the big switch case

--- API/src/neoNodeMsgParser.js
+++ API/src/neoNodeMsgParser.js
@@ -3,18 +3,32 @@
 // JSON string data coming from the
 // NeoCortec gateway
 
+module.exports = {
+    parseData,
+};
 
 /** Function for parsing the turning the data
  * into a JSON object and deciding what to do next.
  * @param data JSONstring the JSON string from the gateway node.
- *
+ * @returns
  */
 async function parseData(data) {
+  var dataObj = JSON.parse(data);
 
-}
+  console.log('DATA: ' + data+ '\n');
+  // If the data is a neighborCall
+  // loops and prints all neighbor node id's.
+  switch (dataObj.objectType) {
+    case "neighborListReply":
+      for (var node of dataObj.neighbors) {
+        console.log('NodeId: ' + node.nodeId+ '\n');
+      }
+      break;
 
+    default:
 
-module.exports = {
-    parseData,
+  }
+  if(dataObj.objectType == "neighborListReply"){
+  }
 }
 
