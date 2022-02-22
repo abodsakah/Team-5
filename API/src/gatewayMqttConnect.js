d25d09550c29c2bb297aef5181dbfed4b39cd194
Noah <nallephu12@gmail.com>
2022-02-22 18:50:10 +0100

    init first gatewayMqttConnect. Needs changes from just being a test

---
+++ API/src/gatewayMqttConnect.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const MQTT = require('async-mqtt');
+
+// connection option
+const options = {
+  clean: true,           // retain session
+  // Authentication information
+  clientId: 'NodeJs-test-client',
+  username: 'flex',
+  password: 'mqtt-flex',
+}
+
+const client = MQTT.connect('mqtt://139.162.146.61:8883', options);
+
+// When passing async functions as event listeners, make sure to have a try
+// catch block
+
+const doStuff =
+    async () => {
+  console.log('Starting');
+  try {
+    // subscribe to topic test
+    await client.subscribe('test');
+    // send message to topic test
+    await client.publish('test', 'It works!');
+    // This line doesn't run until the server responds to the publish
+    await client.end();
+    // This line doesn't run until the client has disconnected without error
+    console.log('Done');
+  } catch (e) {
+    // Do something about it!
+    console.log(e.stack);
+    process.exit();
+  }
+}
+
+                client.on('connect', doStuff);
+
+// event will trigger when a message comes in on a topic we are subscribed too.
+client.on('message', function(topic, message) {
+  // message is Buffer
+  console.log(message.toString());
+  client.end();
+})
+
