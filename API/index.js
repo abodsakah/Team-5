575b6f31b8a31608374c3dbc5b5c2a8fe1c3f542
abodsakka <abodsakka2001@gmail.com>
2022-02-24 11:58:22 +0100

    no changes

--- API/index.js
+++ API/index.js
@@ -7,10 +7,13 @@ const app = express();
 
 const port = process.env.PORT || 9000;
 
+
+
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     console.log(`${req.method} request for '${req.url}'`);
+    
     next();
 });
 
