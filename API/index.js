9f1631fc73fddfe9b73b326c506aa41c1b3d18d8
abodsakka <abodsakka2001@gmail.com>
2022-03-06 12:29:34 +0100

    added gracefulle shutdown to backend

--- API/index.js
+++ API/index.js
@@ -151,6 +151,40 @@ app.get("*", (req, res) => {
     res.status(404).send("Not found");
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
+
+// gracefully shutdown
+
+process.on('SIGTERM', shutDown);
+process.on('SIGINT', shutDown);
+
+let connections = [];
+server.on('connection', connection => {
+    connections.push(connection);
+    connection.on('close', () => {
+        connections = connections.filter(curr => curr !== connection);
+    });
+});
+
+function shutDown() {
+    console.log('Received kill signal, shutting down gracefully');
+    server.close(() => {
+        console.log('Closed out remaining connections');
+        process.exit(0);
+    });
+
+    setTimeout(() => {
+        console.error('Could not close connections in time, forcefully shutting down');
+        process.exit(1);
+    }, 10000);
+
+    setTimeout(() => {
+        console.error('Could not close connections in time, forcefully shutting down');
+        process.exit(1);
+    }, 30000);
+    
+    connections.forEach(curr => curr.end());
+    setTimeout(() => connections.forEach(curr => curr.destroy()), 5000);
+}
