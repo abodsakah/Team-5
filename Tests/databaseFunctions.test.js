396fd80a61e925fdfd84e8461e9cae91de126ad4
abodsakka <abodsakka2001@gmail.com>
2022-04-28 02:18:34 +0200

    writing unit tests with jest

---
+++ Tests/databaseFunctions.test.js
@@ -0,0 +1,25 @@
+jest.setTimeout(60000); // 1 minute 
+const dbConnection = require('../API/src/dbConnection');
+// import {getUserById} from '../API/src/dbConnection';
+
+describe('test getting users by id', () => {
+  test('Tries user id 1 and should return a user with a email of "abodsakka2001@gmail.com"', () => {
+    return dbConnection.getUserById(1).then(data => {
+      expect(data[0].email).toBe('abodsakka2001@gmail.com');
+    });
+  });
+  test('Tries user id 5 and should return nothing', () => {
+    return dbConnection.getUserById(5).then(data => {
+      expect(data.length).toBe(0);
+    });
+  });
+}); 
+
+describe('test getting companies', () => {
+  test('The first company in the list should be Tract', () => {
+    return dbConnection.getCompanies().then(data => {
+      expect(data[0].name).toBe('Tract');
+    });
+  });
+});
+        
