101baf48c90b711c6f545df0ce2c9039cbc0b1d0
Noah Hakansson <noah.hakansson@protonmail.com>
2022-05-16 18:05:07 +0200

    Added option to restart or quit test-mobilix.js CLI program.

--- API/src/test-mobilix.js
+++ API/src/test-mobilix.js
@@ -7,18 +7,25 @@ var mobilix = require('./mobilixApiClient');
 const prompt = require('prompt-sync')();
 
 async function testMobilix() {
-    var workOrders = [];
-    var companyId = prompt('Which company id do you want to see WorkOrders for: ');
-    console.log('WorkOrders:\n');
-    workOrders = await mobilix.getCompanyWorkOrders(companyId);
-    console.log(workOrders);
-    console.log(workOrders.length);
+    var exit = false;
+    while (exit === false) {
+        var workOrders = [];
+        var companyId = prompt('Which company id do you want to see WorkOrders for: ');
+        console.log('WorkOrders:\n');
+        workOrders = await mobilix.getCompanyWorkOrders(companyId);
+        console.log(workOrders);
+        console.log(workOrders.length);
 
-    if (workOrders.length > 0) {
-        var res = prompt('type DELETE if you want to delete all WorkOrders: ');
-        if (res === 'DELETE') {
-            await mobilix.deleteAllWorkOrders();
-            console.log('All WorkOrders have been deleted!!!');
+        if (workOrders.length > 0) {
+            var res = prompt('type DELETE if you want to delete all WorkOrders, otherwise nothing: ');
+            if (res === 'DELETE') {
+                await mobilix.deleteAllWorkOrders();
+                console.log('All WorkOrders have been deleted!!!');
+            }
+        }
+        var exitRes = prompt("If you want to exit type 'q', anything else to start over: ");
+        if (exitRes == 'q') {
+            exit = true;
         }
     }
 }
