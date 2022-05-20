3c81fa4bf802beae1906eccb31b870d32014ad1e
haannaj <73937750+haannaj@users.noreply.github.com>
2022-05-20 07:50:32 +0200

    Added reported status

--- src/components/DeviceTable.jsx
+++ src/components/DeviceTable.jsx
@@ -18,23 +18,31 @@ function DeviceTable({devices}) {
     };
 
     return (
-        <TableContainer component={Paper} style={{boxShadow: 'none'}} >
-            <Table size="small" aria-label="a dense table" style={{ borderCollapse:'separate', borderSpacing: '0 0.7em', boxShadow: 'none' }}>
+        <TableContainer component={Paper} style={{boxShadow: 'none', width: '100%'}} >
+            <Table size="small" aria-label="a dense table" style={{ borderCollapse:'separate', boxShadow: 'none'}}>
                 <TableBody>
                     {devices !== undefined && (
                     <>
                         {devices.map((row) => (
                                 <TableRow key={row.id}>
-                                    <TableCell component="th" scope="row" style={{border: 'none', padding: "7px"}} >
+                                    <TableCell component="th" scope="row" style={{border: 'none', padding: '0'}} >
                                         {/* Card */}
-                                        <Card sx={{display: 'flex', border: '1px solid #e0e0e0', textDecoration: 'none', padding: "0 1em", boxShadow: "none"}} >
+                                        <Card sx={{ display: 'flex', border: '1px solid #e0e0e0', textDecoration: 'none', boxShadow: 'none', margin: '0.8em 0 0.1em 0'}} >
                                             {/* Sensor title */}
-                                            <Box sx={{display: 'flex', flexDirection: 'column', width: '100%', alignContent: 'center'}}>
-                                                <CardContent sx={{padding: '1em 0', display: 'flex', alignContent: 'center'}}>
-                                                    <Typography component="div" variant="h5" sx={{textTransform: 'capitalize', padding: '0'}} >
-                                                        {row.name}
-                                                    </Typography>
-                                                </CardContent>
+                                            <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%'}}>
+                                            <CardContent sx={{ flex: '1 0 auto' }}>
+                                                <Typography component="div" variant="h5" sx={{ textTransform: 'capitalize' }} >
+                                                { row.name }
+                                                </Typography>
+                                                < br />
+                                            </CardContent>
+                                            </Box>
+                                            {/* status */}
+                                            <Box style={{ display: 'flex', flexDirection: 'row', margin: '1.2em auto', textTransform: 'capitalize', alignSelf: 'flex-end'}} >
+                                                <Typography variant="subtitle2" color="text.secondary" component="div">
+                                                { row.status }
+                                                </Typography>
+                                                <CircleRoundedIcon sx={{fontSize: 20, margin: 'auto', color: 'red', padding: "0 0.45em"}} />
                                             </Box>
                                         </Card>
                                     </TableCell>
