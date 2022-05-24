f63c065262089b8f012ee738094a6e2553794041
abodsakka <abodsakka2001@gmail.com>
2022-05-24 18:12:52 +0200

    Tract logo & Admin menu items ~ Fixed Tract logo not changing ~ Fixed menu items for Admins where showing for non admins

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {styled} from '@mui/system'
 import {Button, Card, CircularProgress} from '@mui/material';
 import {useAuth0} from '@auth0/auth0-react';
-
+import TractLogo from './static/images/Asset 1.png';
 
 const Login = ({loading, cookies}) => {
 
@@ -21,7 +21,7 @@ const Login = ({loading, cookies}) => {
   return (
     <LoginContainer style={{height: '100vh'}}>
       <Card style={{padding: '5rem', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-        <img src={require('./static/images/Asset 1.png')} style={{height: '8%', marginBottom: '1rem'}} />
+        <img src={TractLogo} style={{height: '8%', marginBottom: '1rem'}} />
         {loading ? (<><h3>Loading</h3><CircularProgress /></>) :
           (<Button onClick={() => loginWithPopup()} variant="contained" style={{padding: '1rem', width: '100%'}}>Login</Button>)
         }
